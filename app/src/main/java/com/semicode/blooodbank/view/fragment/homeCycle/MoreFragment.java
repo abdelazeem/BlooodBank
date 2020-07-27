91908e747a4992000072fe2372f905d4957c77c2
abdelazeem <abdelazeemmagdi@gmail.com>
2020-07-27 06:14:11 +0200

    welcome

---
+++ app/src/main/java/com/semicode/blooodbank/view/fragment/homeCycle/MoreFragment.java
@@ -0,0 +1,39 @@
+package com.semicode.blooodbank.view.fragment.homeCycle;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.semicode.blooodbank.R;
+import com.semicode.blooodbank.view.fragment.splashCycle.BaseFragment;
+
+import butterknife.ButterKnife;
+
+
+public class MoreFragment extends BaseFragment {
+
+
+
+
+    public MoreFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =inflater.inflate(R.layout.fragment_home_more, container, false);
+        ButterKnife.bind(this, view);
+        return view ;
+    }
+}
